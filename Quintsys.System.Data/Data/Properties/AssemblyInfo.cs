3c47023f15778cba9068fbf65b820c5221d188df
Erich Quintero <qbantek@gmail.com>
2015-10-08 14:07:15 -0400

    initial

---
+++ Quintsys.System.Data/Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Quintsys.System.Data")]
+[assembly: AssemblyDescription("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("QuintSys")]
+[assembly: AssemblyProduct("Quintsys.System.Data")]
+[assembly: AssemblyCopyright("Copyright © QuintSys 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e864cf8f-b088-4f1c-abe4-7a4789dfacc9")]
+[assembly: AssemblyVersion("1.0.*")]
