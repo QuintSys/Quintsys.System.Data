ee220fd6b001ae6ae90681e29d37da6cde88f079
Erich Quintero <qbantek@gmail.com>
2015-10-26 19:54:44 -0400

    latest version of quintsys.system

--- Quintsys.System.Data/Data/Properties/AssemblyInfo.cs
+++ Quintsys.System.Data/Data/Properties/AssemblyInfo.cs
@@ -2,13 +2,8 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Quintsys.System.Data")]
-[assembly: AssemblyDescription("The System.Data namespace provides access to classes that represent the ADO.NET architecture. ADO.NET lets you build components that efficiently manage data from multiple data sources.")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-
+[assembly: AssemblyDescription("Extensions and custom classes for the System.Data namespace.")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("QuintSys")]
 [assembly: AssemblyProduct("Quintsys.System.Data")]
 [assembly: AssemblyCopyright("Copyright © QuintSys 2015")]
@@ -16,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e864cf8f-b088-4f1c-abe4-7a4789dfacc9")]
-[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.5")]
